31490ba618b6c0b9f268908b28196e755e569dae
luky2110 <kemlo2110@gmail.com>
2026-06-18 21:38:19 +0200

    Update hyprland.lua

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -46,7 +46,7 @@ end)
 hl.env("HYPRCURSOR_THEME", "Bibata-Modern-Ice")
 hl.env("HYPRCURSOR_SIZE", "24")
 
--- Nvidia stuff
+-- Better NVIDIA Support
 -- hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
 -- hl.env("GBM_BACKEND", "nvidia-drm")
 -- hl.env("LIBVA_DRIVER_NAME", "nvidia")        
@@ -58,9 +58,9 @@ hl.env("ELECTRON_OZONE_PLATFORM_HINT", "auto")
 hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
 hl.env("QT_QPA_PLATFORM", "wayland;xcb")
 hl.env("QT_QPA_PLATFORMTHEME", "qt5ct")
--- hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
+hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
 
--- xdg desktop stuff
+-- Set XDG variables
 hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
 hl.env("XDG_SESSION_TYPE", "wayland")
 hl.env("XDG_SESSION_DESKTOP", "Hyprland")
