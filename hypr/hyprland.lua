080cb82a5160aa5a2ef42173ee70d739d40a3743
lejlizzle <bbekii858@gmail.com>
2026-06-18 20:31:01 +0200

    Uncomment QT_WAYLAND_DISABLE_WINDOWDECORATION
    Enable QT window decoration setting for Hyprland.

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -60,6 +60,7 @@ hl.env("ELECTRON_OZONE_PLATFORM_HINT", "auto")
 hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
 hl.env("QT_QPA_PLATFORM", "wayland;xcb")
 hl.env("QT_QPA_PLATFORMTHEME", "qt5ct")
+-- hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
 
 -- xdg desktop stuff
 hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
