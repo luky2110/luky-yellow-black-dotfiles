d9d53b29137669fe74c5da970385c55e5b285b55
luky2110 <kemlo2110@gmail.com>
2026-06-18 16:25:59 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -9,4 +9,5 @@ cd luky-yellow-black-dotfiles
 mv alacritty hypr waybar wofi ~/.config
 cd
 rm -rf luky-yellow-black-dotfiles
+chmod +x installer.sh ~/.config/waybar/power.sh
 echo "Luky's Dotfiles were successfully installed"
